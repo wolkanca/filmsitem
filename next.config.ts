a304cb837915bc2b272dd323cb8ecc0cd83e8ad6
wolkanca <wolkanca@gmail.com>
2026-07-03 05:06:01 +0300

    feat: add initial movies data file and configure next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "image.tmdb.org",
-      "images.unsplash.com",
-      "m.media-amazon.com",
-      "ia.media-imdb.com",
-      "img.omdbapi.com",
-      "img.youtube.com",
-      "i0.wp.com",
-      "upload.wikimedia.org",
-      "encrypted-tbn0.gstatic.com"
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 
