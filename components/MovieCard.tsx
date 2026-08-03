5ea5d3bb89917d8512a2851fdf83311aae407de2
wolkanca <wolkanca@gmail.com>
2026-08-03 04:28:33 +0300

    feat: add MovieCard component with hover effects and rating display

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -17,7 +17,7 @@ export default function MovieCard({ movie }: MovieCardProps) {
 
         {/* Poster Image */}
         <PosterImage
-          src={`https://i0.wp.com/izlediklerim.wolkanca.com/${movie.poster}`}
+          src={movie.poster}
           alt={movie.title}
           fill
           sizes="(max-width: 640px) 50vw, (max-width: 1024px) 25vw, 20vw"
