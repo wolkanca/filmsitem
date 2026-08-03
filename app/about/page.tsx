185b44920c0c07e9069490d40177182435014857
wolkanca <wolkanca@gmail.com>
2026-08-03 05:45:58 +0300

    feat: add about page to provide project background and contact information

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -24,7 +24,7 @@ export default function AboutPage() {
               Hakkında
             </h1>
           </div>
-          <p className="m-auto text-3xl leading-8 text-zinc-300 text-center max-w-5xl p-10 mb-10 rounded-3xl border border-white/10 bg-zinc-500/20">
+          <p className="m-auto sm:text-3xl text-2xl leading-8 text-zinc-300 text-center max-w-5xl p-10 mb-10 rounded-3xl border border-white/10 bg-zinc-500/20">
             <strong>İzlediklerim</strong>, yıllardır izlediğim film ve dizileri
             kayıt altına almak, puanlamak ve kendi sinema arşivimi oluşturmak
             amacıyla geliştirdiğim kişisel bir projedir.
