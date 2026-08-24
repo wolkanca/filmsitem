1abebb3708e0387ccb9eccbf344861f24902cca3
wolkanca <wolkanca@gmail.com>
2026-08-24 22:05:55 +0300

    feat: create MovieCard component with hover overlay and rating badges

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -17,7 +17,7 @@ export default function MovieCard({ movie }: MovieCardProps) {
 
         {/* Poster Image */}
         <PosterImage
-          src={`https://alsetek.com/izlediklerim${movie.poster}`}
+          src={movie.poster}
           alt={movie.title}
           fill
           sizes="(max-width: 640px) 50vw, (max-width: 1024px) 25vw, 20vw"
